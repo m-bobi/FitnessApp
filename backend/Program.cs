0f1fdf0214e06b92d4b14bdf0a202f75998eb573
m-bobi <melosbobi12@gmail.com>
2024-03-21 17:35:28 +0100

    + Testing backend

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy  =>
+        {
+            policy.WithOrigins("http://localhost:3000");
+        });
+});
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,6 +24,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
 
 var summaries = new[]
 {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,35 @@
+import React, { useState, useEffect } from 'react';
+
 function App() {
+  const [forecastData, setForecastData] = useState([]);
+
+  useEffect(() => {
+    fetchWeatherForecast();
+  }, []);
+
+  const fetchWeatherForecast = async () => {
+    try {
+      const response = await fetch('http://localhost:5259/weatherforecast');
+      if (!response.ok) {
+        throw new Error('Failed to fetch weather forecast');
+      }
+      const data = await response.json();
+      setForecastData(data);
+    } catch (error) {
+      console.error('Error fetching weather forecast:', error);
+    }
+  };
+
   return (
-    <div className="text-3xl font-bold underline">
-      <h1>Hello World</h1>
+    <div>
+      <h2>Weather Forecast</h2>
+      <ul>
+        {forecastData.map((forecast, index) => (
+          <li key={index}>
+            Date: {forecast.date}, Temperature: {forecast.temperatureC}°C, Summary: {forecast.summary}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
