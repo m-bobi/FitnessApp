713a96e69f35eb1020f42f5ea8229751468a296e
Edoni78 <edonaliu8@gmail.com>
2024-04-13 22:34:32 +0200

    Trainers Crud operations finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect, lazy ,Suspense} from 'react';
 import Home from './pages/Home/Home';
+import './index.css'
 import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 
 function App() {
 
   const Orders = lazy(() => import('./pages/Orders/Orders'))
   const Trainers = lazy(() => import('./pages/Trainers/Trainers'))
+  // const EditTrainers = lazy(() => import('./components/TrainersCrud/EditTrainer'))
 
   return (
       <div>
@@ -15,6 +17,7 @@ function App() {
             <Route exact path="/" element={<Home />} />
             <Route exact path="/orders" element={<Orders />} />
             <Route exact path="/trainers" element={<Trainers />} />
+            {/* <Route exact path="/editTrainer/:id" element={<EditTrainers />} /> */}
 
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
