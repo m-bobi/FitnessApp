a2cffc37385d36f6ab184e023b28682ca91b2dba
Edoni78 <edonaliu8@gmail.com>
2024-04-14 19:38:51 +0200

    Link fixed

--- frontend/src/components/shared/Navbar/DashboardNavigation.jsx
+++ frontend/src/components/shared/Navbar/DashboardNavigation.jsx
@@ -77,7 +77,7 @@ const DashboardNavigation = () => {
             </li>
             <li>
               <Link
-                to="/TrainerCRUD"
+                to="/trainers"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
