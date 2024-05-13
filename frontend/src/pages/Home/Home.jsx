fb20a8b0848f88c6fdf49efa231424d63e51503b
m-bobi <melosbobi12@gmail.com>
2024-05-13 13:45:53 +0200

    + Add title

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/shared/Navbar/Navbar'
+import {Helmet} from "react-helmet"
 
 
 const Home = () => {
   return (
-      <div
-        className="homepage h-[32rem]"
-      >
+      <div className="homepage h-[32rem]">
+    <Helmet>
+      <title>Ascend | Home</title>
+    </Helmet>
         <Navbar/>
       </div>
   );
