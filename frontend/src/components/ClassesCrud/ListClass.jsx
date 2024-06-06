7e067abc3bc818e4599b7d42885769c04d9117e4
m-bobi <melosbobi12@gmail.com>
2024-06-06 11:38:04 +0200

    + Fixed Trainer not listing in admin dashboard.

--- frontend/src/components/ClassesCrud/ListClass.jsx
+++ frontend/src/components/ClassesCrud/ListClass.jsx
@@ -91,23 +91,6 @@ const ListClass = () => {
     setEditedClass({ ...editedClass, [field]: value });
   };
 
-  const matchingTrainers = trainers.filter(
-    (trainer) => trainer.id === classes.trainerId
-  );
-
-  const trainerDisplay =
-    matchingTrainers.length > 0 ? (
-      matchingTrainers.map((trainer) => (
-        <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
-          {trainer.name}
-        </td>
-      ))
-    ) : (
-      <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
-        No trainers assigned
-      </td>
-    );
-
   return (
     <>
       <ToastContainer />
@@ -274,13 +257,13 @@ const ListClass = () => {
                       scope="col"
                       className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
                     >
-                      Description
+                      Type
                     </th>
                     <th
                       scope="col"
                       className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
                     >
-                      Type
+                      Description
                     </th>
                     <th
                       scope="col"
@@ -298,84 +281,98 @@ const ListClass = () => {
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
                   {classes && classes.length > 0 ? (
-                    classes.map((p) => (
-                      <tr className="hover:bg-gray-100 dark:hover:bg-gray-700">
-                        <td className="w-4 p-4">
-                          <div className="flex items-center">
-                            <input
-                              id=""
-                              aria-describedby="checkbox-1"
-                              type="checkbox"
-                              className="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600"
+                    classes.map((classItem) => {
+                      const trainer = trainers.find(
+                        (trainer) => trainer.id === classItem.trainerId
+                      );
+                      return (
+                        <tr
+                          key={classItem.classId}
+                          className="hover:bg-gray-100 dark:hover:bg-gray-700"
+                        >
+                          <td className="w-4 p-4">
+                            <div className="flex items-center">
+                              <input
+                                id={`checkbox-${classItem.classId}`}
+                                aria-describedby="checkbox-1"
+                                type="checkbox"
+                                className="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600"
+                              />
+                              <label
+                                htmlFor={`checkbox-${classItem.classId}`}
+                                className="sr-only"
+                              >
+                                checkbox
+                              </label>
+                            </div>
+                          </td>
+                          <td className="flex items-center p-4 mr-12 space-x-6 whitespace-nowrap">
+                            <img
+                              className="w-10 h-10 rounded-full"
+                              src={`/img/classes/${classItem.classImage}`}
+                              alt="Class"
                             />
-                            <label for="" className="sr-only">
-                              checkbox
-                            </label>
-                          </div>
-                        </td>
-                        <td className="flex items-center p-4 mr-12 space-x-6 whitespace-nowrap">
-                          <img
-                            className="w-10 h-10 rounded-full"
-                            src={`/img/classes/${p.classImage}`}
-                          />
-                          <div className="text-sm font-normal text-gray-500 dark:text-gray-400">
-                            <div className="text-base font-semibold text-gray-900 dark:text-white"></div>
-                            <div className="text-sm font-normal text-gray-500 dark:text-gray-400"></div>
-                          </div>
-                        </td>
-                        <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                          {p.classDescription}
-                        </td>
-                        <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                          {p.classType}
-                        </td>
-                        {trainerDisplay}
-                        <td className="p-4 space-x-2 whitespace-nowrap">
-                          <button
-                            onClick={() => handleEdit(p)}
-                            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                          >
-                            <svg
-                              className="w-4 h-4 mr-2"
-                              fill="currentColor"
-                              viewBox="0 0 20 20"
-                              xmlns="http://www.w3.org/2000/svg"
+                            <div className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                              <div className="text-base font-semibold text-gray-900 dark:text-white"></div>
+                              <div className="text-sm font-normal text-gray-500 dark:text-gray-400"></div>
+                            </div>
+                          </td>
+                          <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {classItem.classType}
+                          </td>
+                          <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {classItem.classDescription}
+                          </td>
+                          <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {trainer ? trainer.name : "No trainers assigned"}
+                          </td>
+                          <td className="p-4 space-x-2 whitespace-nowrap">
+                            <button
+                              onClick={() => handleEdit(classItem)}
+                              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                             >
-                              <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
-                              <path
-                                fillRule="evenodd"
-                                d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-                                clipRule="evenodd"
-                              ></path>
-                            </svg>
-                            Edit class
-                          </button>
-                          <button
-                            onClick={() => handleDelete(p.classId)}
-                            data-modal-target="delete-user-modal"
-                            data-modal-toggle="delete-user-modal"
-                            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900"
-                          >
-                            <svg
-                              className="w-4 h-4 mr-2"
-                              fill="currentColor"
-                              viewBox="0 0 20 20"
-                              xmlns="http://www.w3.org/2000/svg"
+                              <svg
+                                className="w-4 h-4 mr-2"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
+                                <path
+                                  fillRule="evenodd"
+                                  d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                                  clipRule="evenodd"
+                                ></path>
+                              </svg>
+                              Edit class
+                            </button>
+                            <button
+                              onClick={() => handleDelete(classItem.classId)}
+                              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900"
                             >
-                              <path
-                                fillRule="evenodd"
-                                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                clipRule="evenodd"
-                              ></path>
-                            </svg>
-                            Delete class
-                          </button>
-                        </td>
-                      </tr>
-                    ))
+                              <svg
+                                className="w-4 h-4 mr-2"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  fillRule="evenodd"
+                                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                  clipRule="evenodd"
+                                ></path>
+                              </svg>
+                              Delete class
+                            </button>
+                          </td>
+                        </tr>
+                      );
+                    })
                   ) : (
                     <tr>
-                      <td colSpan="2">No Classes at the moment!</td>
+                      <td colSpan="6" className="p-4 text-center text-gray-500">
+                        No Classes at the moment!
+                      </td>
                     </tr>
                   )}
                 </tbody>
